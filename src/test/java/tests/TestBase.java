dd449989994c33a2c1f349d0b13d0b689346e406
Zhanna March <marzhanter@gmail.com>
2022-11-12 22:19:03 +0300

    -addJenkins

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,12 +2,14 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import drivers.BrowserStackMobileDriver;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
-import static com.codeborne.selenide.Selenide.closeWebDriver;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 
 public class TestBase {
 
@@ -19,12 +21,18 @@ public class TestBase {
     }
 
     @BeforeEach
-    public void startDriver() {
+    public void startDriver()
+    {
+        addListener("AllureSelenide", new AllureSelenide());
         open();
     }
 
     @AfterEach
     public void tearDown() {
+        String sessionId = sessionId().toString();
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
         closeWebDriver();
+        Attach.video(sessionId);
     }
 }
